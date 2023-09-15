47d476f932c78592e80ecf88818afbb64c8cbaa2
Allan Kirui <allan.kirui@outlook.com>
2023-09-15 15:24:52 +0300

    Update tailwind screens, extend font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        viga: ["Viga"],
+      },
+      colors: {
+        "navajo-white": "#fedaaa",
+        "mango-tango": "#f28e40",
+        liver: "#6b321e",
+        "sandy-brown": "#f2a05e",
+        "pearl-copper": "#7d3c28",
+      },
+    },
   },
   plugins: [],
 };
