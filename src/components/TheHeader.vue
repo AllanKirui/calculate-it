850a71aa67391074b3eb92aab582c715426bdab2
Allan Kirui <allan.kirui@outlook.com>
2023-09-16 16:08:09 +0300

    Add markup, styles for TheHeader

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- The Header -->
+  <header>
+    <nav class="flex flex-col items-center">
+      <!-- Nav Top -->
+      <div class="flex justify-between">
+        <h1 class="text-5xl">Calculate It!</h1>
+      </div>
+
+      <!-- Nav Bottom -->
+      <div class="mt-3">
+        <ul class="links-wrapper flex gap-4 text-lg">
+          <li><a href="#">Math</a></li>
+          <li><a href="#">Age</a></li>
+          <li><a href="#">BMI</a></li>
+          <li><a href="#">Length</a></li>
+          <li><a href="#">Mass</a></li>
+          <li><a href="#">Temperature</a></li>
+          <li><a href="#">Currency</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
