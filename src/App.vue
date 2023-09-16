33ff638be72bf6892a450f4ed8d4ca0de0db683d
Allan Kirui <allan.kirui@outlook.com>
2023-09-16 16:09:20 +0300

    Add import to TheHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1 class="text-3xl text-green-500">Calculate It!</h1>
+  <!-- Global Container -->
+  <div class="container py-10 max-w-4xl mx-auto">
+    <TheHeader />
+  </div>
 </template>
+
+<script setup>
+import TheHeader from './components/TheHeader.vue';
+</script>
