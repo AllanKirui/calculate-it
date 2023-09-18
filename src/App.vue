7dee7c502eb95b03228c092ccc06715e721a4154
Allan Kirui <allan.kirui@outlook.com>
2023-09-18 15:04:46 +0300

    Add import to TheCalculator cmp

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   <!-- Global Container -->
   <div class="container py-10 max-w-4xl mx-auto">
     <TheHeader />
+    <TheCalculator />
   </div>
 </template>
 
 <script setup>
 import TheHeader from './components/TheHeader.vue';
+import TheCalculator from './components/TheCalculator.vue';
 </script>
