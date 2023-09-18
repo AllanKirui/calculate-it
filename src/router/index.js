75dc6629d4955cf2a7ed5a9334a7f93336cceb60
Allan Kirui <allan.kirui@outlook.com>
2023-09-18 15:41:26 +0300

    Add routes to math and age views

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import ViewMath from "@/views/ViewMath.vue";
+import ViewAge from "@/views/ViewAge.vue";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      redirect: "/math",
+    },
+    {
+      path: "/math",
+      name: "math",
+      component: ViewMath,
+    },
+    {
+      path: "/age",
+      name: "age",
+      component: ViewAge,
+    },
+  ],
 });
 
 export default router;
