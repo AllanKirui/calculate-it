f3ba19d9a0f27080b7d77a419454692147724db0
Allan Kirui <allan.kirui@outlook.com>
2023-09-18 15:12:03 +0300

    Update markup for nav links

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -10,13 +10,58 @@
       <!-- Nav Bottom -->
       <div class="mt-3">
         <ul class="links-wrapper flex gap-4 text-lg">
-          <li><a href="#">Math</a></li>
-          <li><a href="#">Age</a></li>
-          <li><a href="#">BMI</a></li>
-          <li><a href="#">Length</a></li>
+          <li>
+            <a href="#">
+              <img src="../assets/img/calculator.svg" width="24" height="24" alt="">
+              <span class="">Math</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="../assets/img/age.svg" width="24" height="24" alt="">
+              <span class="">Age</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <img src="../assets/img/bmi.svg" width="24" height="24" alt="">
+              <span class="">BMI</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <img src="../assets/img/length.svg" width="24" height="24" alt="">
+              <span class="">Length</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <img src="../assets/img/mass.svg" width="24" height="24" alt="">
+              <span class="">Mass</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <img src="../assets/img/temperature.svg" width="24" height="24" alt="">
+              <span class="">Temperature</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <img src="../assets/img/currency.svg" width="24" height="24" alt="">
+              <span class="">Currency</span>
+            </a>
+          </li>
+
+          <!-- <li><a href="#">Length</a></li>
           <li><a href="#">Mass</a></li>
           <li><a href="#">Temperature</a></li>
-          <li><a href="#">Currency</a></li>
+          <li><a href="#">Currency</a></li> -->
         </ul>
       </div>
     </nav>
