3c5bc2c7c2bd9aaf2a301c4b1e1c23fd8e32419e
Allan Kirui <allan.kirui@outlook.com>
2023-11-20 12:02:13 +0300

    Add logic to provide methods to child components

--- src/components/TheCalculator.vue
+++ src/components/TheCalculator.vue
@@ -6,3 +6,91 @@
     <RouterView />
   </div>
 </template>
+
+<script setup>
+import { provide } from "vue"
+
+/* 
+  Methods
+*/
+const removeCommas = (stringNumber) => {
+  if (!stringNumber.includes(",")) {
+    return parseFloat(stringNumber)
+  }
+
+  let num = ""
+  let stringArr = stringNumber.split(",")
+
+  stringArr.forEach((string) => (num += string))
+
+  return parseFloat(num)
+}
+
+const clearAll = (converter, integerPortion) => {
+  integerPortion.topUnit.value = ""
+  integerPortion.bottomUnit.value = ""
+  converter.topUnitValue = ""
+  converter.bottomUnitValue = ""
+
+  // TODO storeMathDataLocally()
+}
+
+const clearChars = (activeDropdown, converter, integerPortion) => {
+  // remove the last character from the expression shown on the calc display
+  if (activeDropdown.value === "top") {
+    converter.topUnitValue = converter.topUnitValue.slice(0, -1)
+    integerPortion.topUnit.value = converter.topUnitValue
+  } else {
+    converter.bottomUnitValue = converter.bottomUnitValue.slice(0, -1)
+    integerPortion.bottomUnit.value = converter.bottomUnitValue
+  }
+
+  // remove any comma seperators and handle deleting chars from numbers with decimals
+  if (converter.topUnitValue.includes(",")) {
+    removeCommasFromUnitValues(activeDropdown, converter, integerPortion)
+  }
+}
+
+const removeCommasFromUnitValues = (
+  activeDropdown,
+  converter,
+  integerPortion
+) => {
+  let numWithoutCommas,
+    numWithCommas = ""
+
+  if (activeDropdown.value === "top") {
+    numWithoutCommas = removeCommas(converter.topUnitValue).toString()
+  } else {
+    numWithoutCommas = removeCommas(converter.bottomUnitValue).toString()
+  }
+
+  if (numWithoutCommas.includes(".")) {
+    numWithCommas = parseFloat(numWithoutCommas).toLocaleString("en", {
+      maximumFractionDigits: 7
+    })
+  } else {
+    numWithCommas = parseFloat(numWithoutCommas).toLocaleString("en", {
+      maximumFractionDigits: 0
+    })
+  }
+
+  if (activeDropdown.value === "top") {
+    if (converter.topUnitValue.endsWith(".") && !numWithCommas.includes("."))
+      numWithCommas += "."
+
+    converter.topUnitValue = numWithCommas
+    integerPortion.topUnit.value = converter.topUnitValue
+  } else {
+    if (converter.bottomUnitValue.endsWith(".") && !numWithCommas.includes("."))
+      numWithCommas += "."
+
+    converter.bottomUnitValue = numWithCommas
+    integerPortion.bottomUnit.value = converter.bottomUnitValue
+  }
+}
+
+provide("removeCommas", removeCommas)
+provide("clearAll", clearAll)
+provide("clearChars", clearChars)
+</script>
