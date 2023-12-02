0c9ecf14e144dee8e1dc80bfb73ad8ae51f81a96
Allan Kirui <allan.kirui@outlook.com>
2023-12-02 23:34:34 +0300

    Update logic for showing the different units

--- src/components/ui/TheDropdown.vue
+++ src/components/ui/TheDropdown.vue
@@ -25,37 +25,13 @@
       <MenuItems
         class="absolute z-10 mt-2 w-56 origin-top-right rounded-md bg-macaroni-and-cheese shadow-lg ring-2 ring-mellow-apricot ring-opacity-1 focus:outline-none text-base md:text-lg text-seal-brown"
       >
-        <div class="py-1">
+        <div v-for="unit in calcUnits" :key="unit" class="py-1">
           <MenuItem v-slot="{ active }">
             <a
-              @click="$emit('setActiveUnit', 'kg')"
+              @click="$emit('setActiveUnit', unit.shortName)"
               href="#"
               :class="[active ? 'bg-mellow-apricot' : '', 'block px-4 py-2']"
-              >Kilogram kg</a
-            >
-          </MenuItem>
-          <MenuItem v-slot="{ active }">
-            <a
-              @click="$emit('setActiveUnit', 'g')"
-              href="#"
-              :class="[active ? 'bg-mellow-apricot' : '', 'block px-4 py-2']"
-              >Gram g</a
-            >
-          </MenuItem>
-          <MenuItem v-slot="{ active }">
-            <a
-              @click="$emit('setActiveUnit', 'lb')"
-              href="#"
-              :class="[active ? 'bg-mellow-apricot' : '', 'block px-4 py-2']"
-              >Pound lb</a
-            >
-          </MenuItem>
-          <MenuItem v-slot="{ active }">
-            <a
-              @click="$emit('setActiveUnit', 'oz')"
-              href="#"
-              :class="[active ? 'bg-mellow-apricot' : '', 'block px-4 py-2']"
-              >Ounce oz</a
+              >{{ unit.longName }}</a
             >
           </MenuItem>
         </div>
@@ -69,6 +45,7 @@ import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue"
 import { ChevronDownIcon } from "@heroicons/vue/20/solid"
 
 defineProps({
+  calcUnits: { type: Object },
   isActive: { type: Boolean, default: false },
   activeUnit: { type: String, default: "kg" }
 })
