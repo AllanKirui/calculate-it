047a1e2b7f2a57c7a7fb7430ff74f88414c06b32
Allan Kirui <allan.kirui@outlook.com>
2023-12-15 13:11:30 +0300

    Update markup to show extra btn for temp converter

--- src/components/ui/ConverterButtons.vue
+++ src/components/ui/ConverterButtons.vue
@@ -3,18 +3,45 @@
   <button @click="$emit('appendNumber', 7)" class="btn btn-numbers">7</button>
   <button @click="$emit('appendNumber', 8)" class="btn btn-numbers">8</button>
   <button @click="$emit('appendNumber', 9)" class="btn btn-numbers">9</button>
-  <button @click="$emit('clear')" class="btn btn-clear row-span-2">AC</button>
+  <button
+    v-if="mode == 'temp'"
+    @click="$emit('clear')"
+    class="btn btn-clear row-span-1"
+  >
+    AC
+  </button>
+  <button v-else @click="$emit('clear')" class="btn btn-clear row-span-2">
+    AC
+  </button>
 
   <!-- Row 2 Buttons -->
   <button @click="$emit('appendNumber', 4)" class="btn btn-numbers">4</button>
   <button @click="$emit('appendNumber', 5)" class="btn btn-numbers">5</button>
   <button @click="$emit('appendNumber', 6)" class="btn btn-numbers">6</button>
+  <button
+    v-if="mode == 'temp'"
+    @click="$emit('backspace')"
+    class="btn btn-operators row-span-1"
+  >
+    &laquo;
+  </button>
 
   <!-- Row 3 Buttons -->
   <button @click="$emit('appendNumber', 1)" class="btn btn-numbers">1</button>
   <button @click="$emit('appendNumber', 2)" class="btn btn-numbers">2</button>
   <button @click="$emit('appendNumber', 3)" class="btn btn-numbers">3</button>
-  <button @click="$emit('backspace')" class="btn btn-operators row-span-2">
+  <button
+    v-if="mode == 'temp'"
+    @click="$emit('toggleNegativeValue')"
+    class="btn btn-operators row-span-1"
+  >
+    +/-
+  </button>
+  <button
+    v-else
+    @click="$emit('backspace')"
+    class="btn btn-operators row-span-2"
+  >
     &laquo;
   </button>
 
@@ -26,5 +53,6 @@
 </template>
 
 <script setup>
-defineEmits(["appendNumber", "clear", "backspace"])
+defineProps(["mode"])
+defineEmits(["appendNumber", "clear", "backspace", "toggleNegativeValue"])
 </script>
