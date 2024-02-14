7f9b29ad5bc7260ffa13c823d5621ffa3979d0f0
Allan Kirui <allan.kirui@outlook.com>
2024-02-14 15:29:14 +0300

    Update styles for TheCalculator container

--- src/components/TheCalculator.vue
+++ src/components/TheCalculator.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- Calculator Container -->
   <div
-    class="relative max-w-3xl mx-auto h-screen md:h-auto md:mt-3 p-4 bg-navajo-white shadow-xl rounded-xl"
+    class="calculator-height relative max-w-3xl mx-auto md:h-auto md:mt-3 px-4 py-2 bg-navajo-white shadow-xl rounded-xl"
   >
     <RouterView />
   </div>
