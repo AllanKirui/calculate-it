0b7369a522ad5be6d7903c6c23ccf549999bd303
Allan Kirui <allan.kirui@outlook.com>
2024-03-24 17:47:29 +0300

    Update markup, logic for showing unit data

--- src/components/ui/UnitValue.vue
+++ src/components/ui/UnitValue.vue
@@ -4,17 +4,8 @@
     @click="$emit('setActiveDropdown', handleDropdownSelection())"
     class="results-wrapper"
   >
-    <h2
-      class="result"
-      :class="[
-        activeDropdown === 'top' && dropdownOwner === 'top' ? 'active' : '',
-        activeDropdown === 'bottom' && dropdownOwner === 'bottom'
-          ? 'active'
-          : '',
-        unitValue && unitValue.length > 20 ? 'text-2xl' : ''
-      ]"
-    >
-      {{ unitValue || defaultResult }}
+    <h2 class="result" :class="setStyles()">
+      {{ getUnitValue() }}
     </h2>
   </div>
 
@@ -23,19 +14,64 @@
     @click="$emit('setActiveDropdown', handleDropdownSelection())"
     class="unit-name-wrapper"
   >
-    <span>{{ unitName }}</span>
+    <span>{{ getUnitName() }}</span>
   </div>
 </template>
 
 <script setup>
-const props = defineProps([
-  "dropdownOwner",
-  "activeDropdown",
-  "unit-value",
-  "unit-name",
-  "default-result"
-])
+const props = defineProps(["dropdownOwner", "converterData"])
 defineEmits(["setActiveDropdown"])
+
+/*
+  Methods
+*/
+const setStyles = () => {
+  let styles = ""
+
+  if (
+    props.converterData.activeDropdown === "top" &&
+    props.dropdownOwner === "top"
+  )
+    styles += "active"
+
+  if (
+    props.converterData.activeDropdown === "bottom" &&
+    props.dropdownOwner === "bottom"
+  )
+    styles += "active"
+
+  if (props.dropdownOwner === "top") {
+    if (
+      props.converterData.topUnitValue &&
+      props.converterData.topUnitValue.length > 16
+    )
+      styles += " text-2xl"
+  }
+
+  if (props.dropdownOwner === "bottom") {
+    if (
+      props.converterData.bottomUnitValue &&
+      props.converterData.bottomUnitValue.length > 16
+    )
+      styles += " text-2xl"
+  }
+
+  return styles
+}
+
+const getUnitValue = () => {
+  if (props.dropdownOwner === "top")
+    return props.converterData.topUnitValue || props.converterData.defaultResult
+
+  return (
+    props.converterData.bottomUnitValue || props.converterData.defaultResult
+  )
+}
+
+const getUnitName = () => {
+  if (props.dropdownOwner === "top") return props.converterData.topUnitName
+  return props.converterData.bottomUnitName
+}
 
 const handleDropdownSelection = () => {
   if (props.dropdownOwner === "top") return "top"
