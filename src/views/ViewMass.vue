3407957cd62ffd91aae6b940941bfb888dbb09c9
Allan Kirui <allan.kirui@outlook.com>
2024-06-07 01:23:20 +0300

    Add more mass units to converter

--- src/views/ViewMass.vue
+++ src/views/ViewMass.vue
@@ -86,6 +86,10 @@ const numberInput = reactive({
 })
 
 const calcUnits = ref({
+  tonne: {
+    shortName: "t",
+    longName: "Tonne t"
+  },
   kilogram: {
     shortName: "kg",
     longName: "Kilogram kg"
@@ -98,9 +102,21 @@ const calcUnits = ref({
     shortName: "g",
     longName: "Gram g"
   },
+  milligram: {
+    shortName: "mg",
+    longName: "Milligram mg"
+  },
   ounce: {
     shortName: "oz",
     longName: "Ounce oz"
+  },
+  carat: {
+    shortName: "ct",
+    longName: "Carat ct"
+  },
+  stone: {
+    shortName: "st",
+    longName: "Stone st"
   }
 })
 
@@ -121,29 +137,85 @@ const massData = reactive({
 
 // non reactive data object for units and their conversion rates
 const conversionRates = {
+  oneTonne: {
+    tonneEquiv: 1,
+    kilogramEquiv: 1000,
+    gramEquiv: 1000000,
+    milligramEquiv: 1000000000,
+    poundEquiv: 2204.62262,
+    ounceEquiv: 35273.9619,
+    caratEquiv: 5000000,
+    stoneEquiv: 157.473044
+  },
   oneKilo: {
     kilogramEquiv: 1,
     gramEquiv: 1000,
     poundEquiv: 2.20462262,
-    ounceEquiv: 35.2739619
+    ounceEquiv: 35.2739619,
+    tonneEquiv: 0.001,
+    milligramEquiv: 1000000,
+    caratEquiv: 5000,
+    stoneEquiv: 0.157473044
   },
   oneGram: {
     gramEquiv: 1,
     kilogramEquiv: 0.001,
     poundEquiv: 0.00220462262,
-    ounceEquiv: 0.0352739619
+    ounceEquiv: 0.0352739619,
+    tonneEquiv: 0.000001,
+    milligramEquiv: 1000,
+    caratEquiv: 5,
+    stoneEquiv: 0.000157473044
+  },
+  oneMilligram: {
+    milligramEquiv: 1,
+    kilogramEquiv: 0.000001,
+    poundEquiv: 0.00000220462262,
+    ounceEquiv: 0.0000352739619,
+    tonneEquiv: 0.000000001,
+    gramEquiv: 0.001,
+    caratEquiv: 0.005,
+    stoneEquiv: 0.000000157473044
   },
   onePound: {
     poundEquiv: 1,
     kilogramEquiv: 0.45359237,
     gramEquiv: 453.59237,
-    ounceEquiv: 16
+    ounceEquiv: 16,
+    tonneEquiv: 0.00045359237,
+    milligramEquiv: 453592.37,
+    caratEquiv: 2267.96185,
+    stoneEquiv: 0.0714285714
   },
   oneOunce: {
     ounceEquiv: 1,
     poundEquiv: 0.0625,
     kilogramEquiv: 0.0283495231,
-    gramEquiv: 28.3495231
+    gramEquiv: 28.3495231,
+    tonneEquiv: 0.0000283495231,
+    milligramEquiv: 28349.5231,
+    caratEquiv: 141.747616,
+    stoneEquiv: 0.00446428571
+  },
+  oneCarat: {
+    caratEquiv: 1,
+    poundEquiv: 0.000440924524,
+    kilogramEquiv: 0.0002,
+    gramEquiv: 0.2,
+    tonneEquiv: 0.0000002,
+    milligramEquiv: 200,
+    ounceEquiv: 0.00705479239,
+    stoneEquiv: 0.0000314946089
+  },
+  oneStone: {
+    stoneEquiv: 1,
+    poundEquiv: 14,
+    kilogramEquiv: 6.35029318,
+    gramEquiv: 6350.29318,
+    tonneEquiv: 0.00635029318,
+    milligramEquiv: 6350293.18,
+    ounceEquiv: 224,
+    caratEquiv: 31751.4659
   }
 }
 
@@ -160,6 +232,18 @@ watch(
         break
       case "oz":
         massData.topUnitName = "Ounce"
+        break
+      case "t":
+        massData.topUnitName = "Tonne"
+        break
+      case "mg":
+        massData.topUnitName = "Milligram"
+        break
+      case "ct":
+        massData.topUnitName = "Carat"
+        break
+      case "st":
+        massData.topUnitName = "Stone"
         break
       default:
         massData.topUnitName = "Kilogram"
@@ -205,6 +289,18 @@ watch(
         break
       case "oz":
         massData.bottomUnitName = "Ounce"
+        break
+      case "t":
+        massData.bottomUnitName = "Tonne"
+        break
+      case "mg":
+        massData.bottomUnitName = "Milligram"
+        break
+      case "ct":
+        massData.bottomUnitName = "Carat"
+        break
+      case "st":
+        massData.bottomUnitName = "Stone"
         break
       default:
         massData.bottomUnitName = "Pound"
@@ -338,6 +434,19 @@ const convertValues = (dropdown, unitValue) => {
         case "oz":
           convertedValue = conversionRates.oneOunce.poundEquiv * activeUnitValue
           break
+        case "t":
+          convertedValue = conversionRates.oneTonne.poundEquiv * activeUnitValue
+          break
+        case "mg":
+          convertedValue =
+            conversionRates.oneMilligram.poundEquiv * activeUnitValue
+          break
+        case "ct":
+          convertedValue = conversionRates.oneCarat.poundEquiv * activeUnitValue
+          break
+        case "st":
+          convertedValue = conversionRates.oneStone.poundEquiv * activeUnitValue
+          break
         default:
           convertedValue = conversionRates.onePound.poundEquiv * activeUnitValue
           break
@@ -354,6 +463,19 @@ const convertValues = (dropdown, unitValue) => {
         case "oz":
           convertedValue = conversionRates.oneOunce.gramEquiv * activeUnitValue
           break
+        case "t":
+          convertedValue = conversionRates.oneTonne.gramEquiv * activeUnitValue
+          break
+        case "mg":
+          convertedValue =
+            conversionRates.oneMilligram.gramEquiv * activeUnitValue
+          break
+        case "ct":
+          convertedValue = conversionRates.oneCarat.gramEquiv * activeUnitValue
+          break
+        case "st":
+          convertedValue = conversionRates.oneStone.gramEquiv * activeUnitValue
+          break
         default:
           convertedValue = conversionRates.onePound.gramEquiv * activeUnitValue
           break
@@ -369,6 +491,19 @@ const convertValues = (dropdown, unitValue) => {
           break
         case "oz":
           convertedValue = conversionRates.oneOunce.ounceEquiv * activeUnitValue
+          break
+        case "t":
+          convertedValue = conversionRates.oneTonne.ounceEquiv * activeUnitValue
+          break
+        case "mg":
+          convertedValue =
+            conversionRates.oneMilligram.ounceEquiv * activeUnitValue
+          break
+        case "ct":
+          convertedValue = conversionRates.oneCarat.ounceEquiv * activeUnitValue
+          break
+        case "st":
+          convertedValue = conversionRates.oneStone.ounceEquiv * activeUnitValue
           break
         default:
           convertedValue = conversionRates.onePound.ounceEquiv * activeUnitValue
@@ -389,9 +524,148 @@ const convertValues = (dropdown, unitValue) => {
           convertedValue =
             conversionRates.oneOunce.kilogramEquiv * activeUnitValue
           break
+        case "t":
+          convertedValue =
+            conversionRates.oneTonne.kilogramEquiv * activeUnitValue
+          break
+        case "mg":
+          convertedValue =
+            conversionRates.oneMilligram.kilogramEquiv * activeUnitValue
+          break
+        case "ct":
+          convertedValue =
+            conversionRates.oneCarat.kilogramEquiv * activeUnitValue
+          break
+        case "st":
+          convertedValue =
+            conversionRates.oneStone.kilogramEquiv * activeUnitValue
+          break
         default:
           convertedValue =
             conversionRates.onePound.kilogramEquiv * activeUnitValue
+          break
+      }
+      break
+    case "t":
+      switch (activeUnit2) {
+        case "kg":
+          convertedValue = conversionRates.oneKilo.tonneEquiv * activeUnitValue
+          break
+        case "g":
+          convertedValue = conversionRates.oneGram.tonneEquiv * activeUnitValue
+          break
+        case "oz":
+          convertedValue = conversionRates.oneOunce.tonneEquiv * activeUnitValue
+          break
+        case "lb":
+          convertedValue = conversionRates.onePound.tonneEquiv * activeUnitValue
+          break
+        case "mg":
+          convertedValue =
+            conversionRates.oneMilligram.tonneEquiv * activeUnitValue
+          break
+        case "ct":
+          convertedValue = conversionRates.oneCarat.tonneEquiv * activeUnitValue
+          break
+        case "st":
+          convertedValue = conversionRates.oneStone.tonneEquiv * activeUnitValue
+          break
+        default:
+          convertedValue = conversionRates.oneTonne.tonneEquiv * activeUnitValue
+          break
+      }
+      break
+    case "mg":
+      switch (activeUnit2) {
+        case "kg":
+          convertedValue =
+            conversionRates.oneKilo.milligramEquiv * activeUnitValue
+          break
+        case "g":
+          convertedValue =
+            conversionRates.oneGram.milligramEquiv * activeUnitValue
+          break
+        case "oz":
+          convertedValue =
+            conversionRates.oneOunce.milligramEquiv * activeUnitValue
+          break
+        case "t":
+          convertedValue =
+            conversionRates.oneTonne.milligramEquiv * activeUnitValue
+          break
+        case "lb":
+          convertedValue =
+            conversionRates.onePound.milligramEquiv * activeUnitValue
+          break
+        case "ct":
+          convertedValue =
+            conversionRates.oneCarat.milligramEquiv * activeUnitValue
+          break
+        case "st":
+          convertedValue =
+            conversionRates.oneStone.milligramEquiv * activeUnitValue
+          break
+        default:
+          convertedValue =
+            conversionRates.oneMilligram.milligramEquiv * activeUnitValue
+          break
+      }
+      break
+    case "ct":
+      switch (activeUnit2) {
+        case "kg":
+          convertedValue = conversionRates.oneKilo.caratEquiv * activeUnitValue
+          break
+        case "g":
+          convertedValue = conversionRates.oneGram.caratEquiv * activeUnitValue
+          break
+        case "oz":
+          convertedValue = conversionRates.oneOunce.caratEquiv * activeUnitValue
+          break
+        case "t":
+          convertedValue = conversionRates.oneTonne.caratEquiv * activeUnitValue
+          break
+        case "mg":
+          convertedValue =
+            conversionRates.oneMilligram.caratEquiv * activeUnitValue
+          break
+        case "lb":
+          convertedValue = conversionRates.onePound.caratEquiv * activeUnitValue
+          break
+        case "st":
+          convertedValue = conversionRates.oneStone.caratEquiv * activeUnitValue
+          break
+        default:
+          convertedValue = conversionRates.oneCarat.caratEquiv * activeUnitValue
+          break
+      }
+      break
+    case "st":
+      switch (activeUnit2) {
+        case "kg":
+          convertedValue = conversionRates.oneKilo.stoneEquiv * activeUnitValue
+          break
+        case "g":
+          convertedValue = conversionRates.oneGram.stoneEquiv * activeUnitValue
+          break
+        case "oz":
+          convertedValue = conversionRates.oneOunce.stoneEquiv * activeUnitValue
+          break
+        case "t":
+          convertedValue = conversionRates.oneTonne.stoneEquiv * activeUnitValue
+          break
+        case "mg":
+          convertedValue =
+            conversionRates.oneMilligram.stoneEquiv * activeUnitValue
+          break
+        case "ct":
+          convertedValue = conversionRates.oneCarat.stoneEquiv * activeUnitValue
+          break
+        case "lb":
+          convertedValue = conversionRates.onePound.stoneEquiv * activeUnitValue
+          break
+        default:
+          convertedValue = conversionRates.oneStone.stoneEquiv * activeUnitValue
           break
       }
       break
