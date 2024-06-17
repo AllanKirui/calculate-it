9f71fefe01be37a20d50661a3aed49da76c36f2e
Allan Kirui <allan.kirui@outlook.com>
2024-06-17 13:53:59 +0300

    Add markup, styles for about page

---
+++ src/views/ViewAbout.vue
@@ -0,0 +1,52 @@
+<template>
+  <div
+    class="relative grid grid-cols-1 grid-rows-2 p-4 md:p-6 text-base sm:text-lg md:text-xl text-center"
+  >
+    <!-- About Text -->
+    <div class="row-start-1 max-w-md md:max-w-xl mx-auto">
+      <h1 class="text-4xl">Calculate It!</h1>
+      <p class="mt-4">
+        From concept to code, creating this calculator app with
+        <strong>Vue's Composition API</strong> was a rewarding journey.
+      </p>
+      <p class="mt-3">
+        Having built previous <strong>Vue</strong> apps using the
+        <strong>Options API</strong>, I wanted to gain hands-on experience with
+        the <strong>Composition API</strong>.
+      </p>
+      <p class="mt-3">Enjoy crunching numbers as much as I did making it!</p>
+    </div>
+
+    <!-- About Links -->
+    <div class="row-start-2 max-w-md md:max-w-xl mx-auto mt-auto">
+      <p>
+        Designed and coded by
+        <a
+          class="bg-mellow-apricot px-1 hover:bg-neon-carrot duration-200 rounded"
+          href="https://allanKirui.github.io"
+          target="_blank"
+          title="Allan's Portfolio"
+          >Allan Kirui</a
+        >
+        in
+        <a
+          class="bg-mellow-apricot px-1 hover:bg-neon-carrot duration-200 rounded"
+          href="https://vuejs.org"
+          target="_blank"
+          title="Vue.js Website"
+          >Vue.js</a
+        >.
+      </p>
+      <p class="mt-1">
+        Inspired by this
+        <a
+          class="bg-mellow-apricot px-1 hover:bg-neon-carrot duration-200 rounded"
+          href="https://play.google.com/store/apps/details?id=com.miui.calculator"
+          target="_blank"
+          title="Calculator App"
+          >Calculator</a
+        >.
+      </p>
+    </div>
+  </div>
+</template>
