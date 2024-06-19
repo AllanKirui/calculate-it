a55e720c50bb159f6cca9ee57dd3978971d4c7b2
Allan Kirui <allan.kirui@outlook.com>
2024-06-19 04:07:37 +0300

    Remove prior fixes for dynamic routing

--- vue.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from "vite"
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/calculate-it/" : "/"
-})
