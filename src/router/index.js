62fa8b2545f0851becda3725b30b6d3ab12a7e98
Allan Kirui <allan.kirui@outlook.com>
2024-06-22 00:51:10 +0300

    Update route config

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router"
 import ViewMath from "@/views/ViewMath.vue"
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory("/calculate-it/"),
   routes: [
     {
       path: "/",
