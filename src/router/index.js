aa8b9dbdaf4ddddc87970f1d07bcba219ec5886e
Allan Kirui <allan.kirui@outlook.com>
2024-06-22 16:00:32 +0300

    Remove prior attempts to fix gh pages dynamic routing

--- src/router/index.js
+++ src/router/index.js
@@ -3,57 +3,53 @@ import { createRouter, createWebHistory } from "vue-router"
 import ViewMath from "@/views/ViewMath.vue"
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      redirect: "/calculate-it/math"
+      redirect: "/math"
     },
     {
-      path: "/calculate-it/",
-      redirect: "/calculate-it/math"
-    },
-    {
-      path: "/calculate-it/math",
+      path: "/math",
       name: "math",
       component: ViewMath
     },
     {
-      path: "/calculate-it/bmi",
+      path: "/bmi",
       name: "bmi",
       component: () => import("../views/ViewBMI.vue")
     },
     {
-      path: "/calculate-it/mass",
+      path: "/mass",
       name: "mass",
       component: () => import("../views/ViewMass.vue")
     },
     {
-      path: "/calculate-it/length",
+      path: "/length",
       name: "length",
       component: () => import("../views/ViewLength.vue")
     },
     {
-      path: "/calculate-it/time",
+      path: "/time",
       name: "time",
       component: () => import("../views/ViewTime.vue")
     },
     {
-      path: "/calculate-it/speed",
+      path: "/speed",
       name: "speed",
       component: () => import("../views/ViewSpeed.vue")
     },
     {
-      path: "/calculate-it/temperature",
+      path: "/temperature",
       name: "temperature",
       component: () => import("../views/ViewTemp.vue")
     },
     {
-      path: "/calculate-it/about",
+      path: "/about",
       name: "about",
       component: () => import("../views/ViewAbout.vue")
     },
-    { path: "/calculate-it/:pathMatch(.*)*", redirect: "/calculate-it/math" }
+    { path: "/:pathMatch(.*)*", redirect: "/math" }
   ]
 })
 
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/calculate-it/",
   plugins: [vue()],
   resolve: {
     alias: {
